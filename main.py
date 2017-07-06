e4dd0ec704a65c8de5256144349cb84dec04f9b9
Chris Lau <chrislyc1991@gmail.com>
2017-07-06 15:18:25 -0600

    changed NOde and main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import Node.py
 #code
 class board:
-	def _init_()
+	def __init__()
 		self.DLL = '' #need to create a DLL 
 	def add_node()
+		
 		#add nodes to create a board
 		#add connections between nodes		
 	def 
