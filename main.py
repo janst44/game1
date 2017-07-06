481971859131435fb3ae9aa01a39106534cadba2
Chris Lau <chrislyc1991@gmail.com>
2017-07-06 16:06:17 -0600

    transfered functions from main to CAL

--- main.py
+++ main.py
@@ -1,34 +1,8 @@
 import Node.py
-#code
-class board:
-	def __init__(self):
-		self.DLL = '' #need to create a DLL 
-	def add_node():
-		
-		#add nodes to create a board
-		#add connections between nodes		
-	def 
-class player:
-	def __init__(self, name_in):
-		self.name = name_in
-		self.at_node = 0
-	def change_node(node_in)
-		self.at_node = node_in
-		
 
-def create_board():
-	#implement the constraction of randomly generated board
-	return
+#implement menu
 
-class dice:
-	def __init__(self):
-	
-	def roll():
-		
 
-def run_board():
-	#include rolling dice, having players take turns, 
-	return
 #GUI
-
+ #use the pygame library or something
 
