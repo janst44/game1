d974cac633d6ba0afb037d585657d812e8536ede
Chris Lau <chrislyc1991@gmail.com>
2017-07-06 15:59:13 -0600

    updated main

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import Node.py
 #code
 class board:
-	def __init__()
+	def __init__(self):
 		self.DLL = '' #need to create a DLL 
-	def add_node()
+	def add_node():
 		
 		#add nodes to create a board
 		#add connections between nodes		
 	def 
+class player:
+	def __init__(self, name_in):
+		self.name = name_in
+		self.at_node = 0
+	def change_node(node_in)
+		self.at_node = node_in
+		
+
 def create_board():
-	
+	#implement the constraction of randomly generated board
 	return
 
+class dice:
+	def __init__(self):
+	
+	def roll():
+		
 
+def run_board():
+	#include rolling dice, having players take turns, 
+	return
 #GUI
 
 
