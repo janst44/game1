b838547829bba58c58804d8bcbf398b57079118b
Chris Lau <chrislyc1991@gmail.com>
2017-07-06 17:13:24 -0600

    GUI cropped image

--- GUI.py
+++ GUI.py
@@ -1,9 +1,11 @@
 import pygame
+
 pygame.init()
-screen = pygame.display.set_mode((400,300))
+screen = pygame.display.set_mode((805,658))
 done = False
 
 img = pygame.image.load('Chutes&Ladders1.gif')
+img = pygame.transform.scale(img, (650,658))
 while not done:
 	screen.blit(img, (0,0))
 	for event in pygame.event.get():
